4e866f254547db11a87c1c22395906cc22db37f7
Soon_Coding <m3088787@naver.com>
2024-01-05 15:55:37 +0900

    [FEAT] 인앱 브라우저 우회 테스트 배포1

--- capstone-design/src/pages/SignIn.jsx
+++ capstone-design/src/pages/SignIn.jsx
@@ -105,16 +105,9 @@ const SignIn = () => {
 
   ////////// 마운트
   useEffect(() => {
-    // 인앱 브라우저로 입장 시 웹 브라우저로 변경
-    // var target_url = 'https://capstone-design-nine.vercel.app/';
-    // window.location.href = 'kakaotalk://web/openExternal?url='+encodeURIComponent(target_url); 
-    // 쿠키 보유 여부 확인
-    isHaveCookie();
+    isHaveCookie(); // 쿠키 보유 여부 확인
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  // var target_url = "https://ldk1009.github.io/CapstoneDesign/";
-  // window.location.href = "kakaotalk://web/openExternal?url=" + encodeURIComponent(target_url);
 
   //////////////////////////////////////////////////렌더링//////////////////////////////////////////////////
   return (
