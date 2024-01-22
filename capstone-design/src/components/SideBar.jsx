71c061b4ac20fc73fb986919f6af1efe1e0216b9
Soon_Coding <m3088787@naver.com>
2024-01-22 10:22:41 +0900

    [DESIGN] 사이드바 디자인 수정
    - 학생증 별도 컴포넌트로 변경
    - 아이콘 색상 및 너비값 변경
    - 메뉴 텍스트 폰트 변경

--- capstone-design/src/components/SideBar.jsx
+++ capstone-design/src/components/SideBar.jsx
@@ -12,14 +12,11 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import MenuIcon from "@mui/icons-material/Menu";
 import HelpIcon from "@mui/icons-material/Help";
 import { useNavigate } from "react-router-dom";
-import { db } from "../firebaseConfig";
-import { useState } from "react";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect } from "react";
 import styled from "styled-components";
 import { Divider } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import LogoutIcon from "@mui/icons-material/Logout";
+import StudentCard from "./StudentCard";
 
 export default function SideBar() {
   ////////// 쿠키 가져오기
@@ -43,7 +40,6 @@ export default function SideBar() {
 
   ////////// uid 쿠키 값 가져오기
   const uidCookieValue = getCookie("uid");
-
 
   const navigate = useNavigate(); // 네비게이트 변수
 
@@ -129,12 +125,12 @@ export default function SideBar() {
         {["홈", "내 정보", "내 게시물"].map((text, index) => (
           <ListItem key={text} disablePadding onClick={() => firstDividerOption(index)}>
             <ListItemButton>
-              <ListItemIcon>
+              <StyledListItemIcon>
                 {index === 0 && <HomeIcon />}
                 {index === 1 && <AccountCircleIcon />}
                 {index === 2 && <DescriptionIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+              </StyledListItemIcon>
+              <StyledListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -142,45 +138,18 @@ export default function SideBar() {
         {["공지사항", "도움말", "로그아웃"].map((text, index) => (
           <ListItem key={text} disablePadding onClick={() => secondDividerOption(index)}>
             <ListItemButton>
-              <ListItemIcon>
+              <StyledListItemIcon>
                 {index === 0 && <NotificationsIcon />}
                 {index === 1 && <HelpIcon />}
                 {index === 2 && <LogoutIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+              </StyledListItemIcon>
+              <StyledListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
     </Box>
   );
-
-  // 유저아이디를 기반으로 회원 정보 가져오기
-  const [userData, setUserData] = useState({
-    uid: "",
-    name: "",
-    email: "",
-    gender: "",
-    age: "",
-    people: "",
-    major: "",
-    number: "",
-  });
-
-  ////////// 유저 정보 불러오기
-  const readUserInfo = async () => {
-    // DB에 문서명이 uid인 문서가 있는지 확인하고 있다면 해당 유저 정보를 가져오기
-    const docRef = doc(db, "users", uidCookieValue);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      setUserData(() => docSnap.data());
-    } else {
-      // docSnap.data() will be undefined in this case
-      alert("회원정보를 찾을 수 없습니다.");
-      navigate("/");
-    }
-  };
 
   ////////// 로그아웃(쿠키 삭제하기)
   const logout = (cookieName) => {
@@ -193,23 +162,13 @@ export default function SideBar() {
     navigate("/");
   };
 
-  ////////// 마운트
-  useEffect(() => {
-    readUserInfo();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   //////////////////////////////////////////////////렌더링//////////////////////////////////////////////////
   return (
     <>
       <StyledMenuIcon onClick={toggleDrawer("right", true)} />
       <Drawer anchor={"right"} open={state["right"]} onClose={toggleDrawer("right", false)}>
         <StudentCardContainer>
-          <StudentCardTop>SCH</StudentCardTop>
-          <StudentCardText>{userData.name}</StudentCardText>
-          <StudentCardText>{userData.major}</StudentCardText>
-          <StudentCardText>
-            {userData.gender} / {userData.age}세
-          </StudentCardText>
+          <StudentCard />
         </StudentCardContainer>
         <Divider />
         {list("right")}
@@ -227,30 +186,34 @@ const StyledMenuIcon = styled(MenuIcon)`
   }
   &:hover {
     cursor: pointer;
-    opacity:0.9;
+    opacity: 0.9;
   }
 `;
 
 const StudentCardContainer = styled.div`
-  border: 1px solid black;
-  border-radius: 10px;
-  margin: 20px 30px;
-  text-align: center;
-  overflow: hidden;
-`;
-
-const StudentCardTop = styled.div`
-  background-color: #26539c;
-  height: 30px;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: white;
-  font-size: 20px;
-  font-weight: bold;
-  font-family: Arial, Helvetica, sans-serif;
+  width: 100%;
+  height: 180px;
 `;
 
-const StudentCardText = styled.div`
-  margin: 5px;
+const StyledListItemIcon = styled(ListItemIcon)`
+  /* 아이콘 컨테이너 크기 */
+  &.MuiListItemIcon-root {
+    min-width: 24px;
+    width: 24px;
+    margin-right: 20px;
+  }
+  /* 아이콘 색상 */
+  & .MuiSvgIcon-root {
+    color: #1c9ad6;
+  }
+`;
+
+const StyledListItemText = styled(ListItemText)`
+  /* 버튼 텍스트 폰트 */
+  & .MuiTypography-root {
+    font-family: "Pretendard-Regular";
+  }
 `;
